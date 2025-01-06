4ad5219ff427fa4389883d6e5737edb65f5e7936
howellea <howelleddy@gmail.com>
2025-01-05 23:52:54 -0600

    created starter code

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+\c employee_db;
+
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+
+);
+
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+
+);
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+
+);
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+
+import { QueryResult } from 'pg';
+import { pool, connectToDb } from './connection.js';
+
+await connectToDb();
